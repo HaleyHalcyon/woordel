ad2a30bc5d11219e4d38245b1a63df173270925e
Haley Halcyon <haleyhalcyon@tuta.io>
2025-03-19 16:07:10 +0900

    Update save.js

--- docs/js/save.js
+++ docs/js/save.js
@@ -1,7 +1,7 @@
 const PREFIX = "WDL_";
 // just do everything in local timezone i can’t be assed
 let now = new Date();
-let daysSinceUnixEpoch = new BigInt(
+let daysSinceUnixEpoch = BigInt(
   Math.floor(now - new Date(0)) / (new Date(1970, 0, 2) - new Date(1970, 0, 1))
 );
 
