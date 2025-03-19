6060e8cee4f129990b525c9c5bfa764a87662d99
Haley Halcyon <haleyhalcyon@tuta.io>
2025-03-19 13:25:38 +0900

    Update save.js

--- docs/js/save.js
+++ docs/js/save.js
@@ -1,15 +1,21 @@
 const PREFIX = "WDL_";
+// just do everything in local timezone i can’t be assed
+let now = new Date();
+let daysSinceUnixEpoch = Math.floor(now - new Date(0)) / (new Date(1970, 0, 2) - new Date(1970, 0, 1));
 
 export function firstTime() {
   const lastPlayed = window.localStorage.getItem(PREFIX + "lastPlayed", null);
   if (lastPlayed == null) {
-    return true;
+    return true; // we’ll eventually set lastPlayed somewhere else
   }
   return false;
 }
 
+export function getDaysSinceUnixEpoch() {
+  return daysSinceUnixEpoch;
+}
+
 export function hasPlayedToday() {
-  const now = new Date();
   const nowString = `${now.getFullYear()}${("0" + now.getMonth() + 1).slice(-2)}${("0" + now.getDate()).slice(-2)}`;
   const lastPlayed = window.localStorage.getItem(PREFIX + "lastPlayed", "00000000");
   console.debug("nowString", nowString, "lastPlayed", lastPlayed);
